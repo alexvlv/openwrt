857d62d4e144d4e83cb41ddca8fdb711b330e9af
alexvlv <alexey.v.v@mail.ru>
2021-01-15 19:57:07 +0300

    Build script - log to logs dir

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 # $Id$
 
+mkdir -p logs
+
 BUILD_START=$(date +"%d/%m/%Y %H:%M")
-echo "Build start at $BUILD_START"  | tee build.log 
-ionice -c 3 nice -n19 make -j$(($(nproc)+1)) V=s 2>&1 | tee -a build.log 
+echo "Build start at $BUILD_START"  | tee logs/build.log 
+ionice -c 3 nice -n19 make -j$(($(nproc)+1)) V=s 2>&1 | tee -a logs/build.log
 BUILD_END=$(date +"%d/%m/%Y %H:%M")
-echo "Build start at $BUILD_START" | tee -a build.log 
-echo "Build done  at $BUILD_END" | tee -a build.log 
+echo "Build start at $BUILD_START" | tee -a logs/build.log
+echo "Build done  at $BUILD_END" | tee -a logs/build.log
