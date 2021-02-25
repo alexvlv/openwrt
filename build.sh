d1a3fb19c547a0863d0ef716c2f0dbb7ab20b84b
Alexey Voloshin <alexvol@local.yurion.ru>
2021-02-25 10:54:51 +0300

    build script: display banner after build

--- build.sh
+++ build.sh
@@ -8,5 +8,9 @@ BUILD_START=$(date +"%d/%m/%Y %H:%M")
 echo "Build start at $BUILD_START"  | tee logs/build.log 
 ionice -c 3 nice -n19 make -j$(($(nproc)+1)) V=s 2>&1 | tee -a logs/build.log
 BUILD_END=$(date +"%d/%m/%Y %H:%M")
+echo
+cat ./firmware/banner
+cat ./firmware/firmware.txt
+echo
 echo "Build start at $BUILD_START" | tee -a logs/build.log
 echo "Build done  at $BUILD_END" | tee -a logs/build.log
